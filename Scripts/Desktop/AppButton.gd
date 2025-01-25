864b2bd09df92e731b0eb43a7911a445b6e28364
mindcool24 <thezachm24@gmail.com>
2025-01-25 02:24:53 -0700

    I think it works, the windows move wrong tho

---
+++ Scripts/Desktop/AppButton.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+@export var Application: PackedScene
+@export var WindowHolder: Node2D
+
+var pressed: bool = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_released("click") and pressed:
+		pressed = false
+		print("Creating window")
+		var instance = Application.instantiate()
+		WindowHolder.add_child(instance)
+	pass
+
+
+func _on_button_area_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if Input.is_action_just_pressed("click") and self.visible == true:
+		# Animation here
+		pressed = true
+		pass
+	pass # Replace with function body.
