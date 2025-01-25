a39c285373b288a14b9ca5917207d12ba0ad6006
mindcool24 <thezachm24@gmail.com>
2025-01-25 01:27:12 -0700

    Things work now

--- Scripts/Desktop/Desktop.gd
+++ Scripts/Desktop/Desktop.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 @export var Background: Sprite2D
 @export var Bar: Sprite2D
-@export var BarButton: Sprite2D
+@export var BarButton: AnimatedSprite2D
 @export var BarMenu: Sprite2D
 
 # Called when the node enters the scene tree for the first time.
@@ -11,7 +11,9 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 func _input(event: InputEvent) -> void:
-	pass
+	if Input.is_action_just_released("click") and BarButton.frame == 1:
+		BarButton.frame = 0
+		BarMenu.visible = !BarMenu.visible
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -19,6 +21,5 @@ func _process(delta: float) -> void:
 
 
 func _on_button_box_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventMouseButton and event.is_pressed():
-		BarMenu.visible = !BarMenu.visible
-	pass # Replace with function body.
+	if Input.is_action_just_pressed("click"):
+		BarButton.frame = 1
