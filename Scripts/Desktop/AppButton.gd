32a900d25d592b00c6911ad78e579b1f87680327
mindcool24 <thezachm24@gmail.com>
2025-01-25 07:28:21 -0700

    I am attempting to do comments now

--- Scripts/Desktop/AppButton.gd
+++ Scripts/Desktop/AppButton.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
+# Application to open
 @export var Application: PackedScene
+
+# Thing that holds the windows created by the button
 @export var WindowHolder: Node2D
 
 var pressed: bool = false
@@ -10,15 +13,17 @@ var pressed: bool = false
 func _ready() -> void:
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	# If the mouse is released and it was pressed, make it not that and also
+	# create an instance of the application defined in the export var
+	# then add that instance as a child
 	if Input.is_action_just_released("click") and pressed:
 		pressed = false
 		var instance = Application.instantiate()
 		WindowHolder.add_child(instance)
 	pass
 
-
+# Check if the button is pressed, make it pressed, then make the menu dissapear
 func _on_button_area_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if Input.is_action_just_pressed("click") and self.visible == true:
 		# Animation here
--- Scripts/Desktop/Desktop.gd
+++ Scripts/Desktop/Desktop.gd
@@ -6,14 +6,20 @@ extends Node2D
 @export var BarButton: AnimatedSprite2D
 @export var BarMenu: Sprite2D
 @export var Settings: Button
+@export var Score: Label
 
-# Called when the node enters the scene tree for the first time.
+var currScore = 0
+
+# Setting up initial things, don't worry about it
+# just don't touch it
 func _ready() -> void:
 	get_viewport().physics_object_picking_first_only = true
 	get_viewport().physics_object_picking_sort = true
 	BarMenu.visible = false
 	pass # Replace with function body.
 
+# If the button is currently pressed (the animation), release it and then toggle
+# the bar menu
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_released("click") and BarButton.frame == 1:
 		BarButton.frame = 0
@@ -21,14 +27,15 @@ func _input(event: InputEvent) -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	$Score.text = str(currScore)
 	pass
 
-
+# Start the animation for the Mindows button press
 func _on_button_box_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if Input.is_action_just_pressed("click"):
 		BarButton.frame = 1
 
-
+# Don't worry about this, just some image fuckary
 func _on_file_dialog_file_selected(path: String) -> void:
 	var image = Image.load_from_file(path)
 	var texture = ImageTexture.create_from_image(image)
@@ -36,7 +43,7 @@ func _on_file_dialog_file_selected(path: String) -> void:
 	$Background.texture = texture
 	pass # Replace with function body.
 
-
+# Open up settings menu
 func _on_settings_pressed() -> void:
 	$FileDialog.visible = true
 	BarMenu.visible = false
